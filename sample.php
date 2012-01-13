c1d3aa46e8e0cd15319fbb55518b1f2cd4dd2a96
Amauris De Jesus <amauris@peekyou.com>
2012-01-13 16:57:49 -0500

    Updated sample code

--- sample.php
+++ sample.php
@@ -3,9 +3,10 @@
 require_once("peekyou_api.php");
 $test=new peekyou();
 $test->set_key("YOUR API KEY GOES HERE");
+$test->set_app_id("YOUR APP ID GOES HERE");
 //In seconds.
 $test->set_frequency(1);
 //second parameter for get_url can be xml,json,or stdclass
-print_r($test->get_url("http://twitter.com/husasdfsaeymichael","xml"));
-
+print_r($test->get_social_audience_info("www.twitter.com/michaelhussey","json"));
+print_r($test->get_social_consumer_info("www.twitter.com/michaelhussey","json"));
 ?>
